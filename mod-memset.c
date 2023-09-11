97ac979120a077850402d6607ca274104ac79eda
falkTX <falktx@falktx.com>
2023-09-11 14:05:28 +0200

    Add an alternative to memset, for posterity
    Signed-off-by: falkTX <falktx@falktx.com>

--- mod-memset.c
+++ mod-memset.c
@@ -100,6 +100,17 @@ void* mod_memset(void* dstpp, int c, size_t len)
     return dstpp;
 }
 
+/* some possible alternatives to memset */
+void* mod_memset_alternative_1(void* dstpp, int c, size_t len)
+{
+    volatile unsigned char *volatile vdstpp = dstpp;
+
+    for (size_t i = 0U; i < len; ++i)
+        vdstpp[i] = 0U;
+
+    return dstpp;
+}
+
 __attribute__((weak))
 void _dummy_symbol_to_prevent_memzero_being_optimized_away(void* pnt, size_t len)
 {
